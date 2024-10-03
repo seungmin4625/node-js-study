682f202bd2dcd93e9edf253ddf8628a222541636
Seungmin Park <seungmin4625@gmail.com>
2024-10-03 16:00:29 +0900

    updated

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize("node-complete", "root", "qktod!23", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: "qktod!23",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
